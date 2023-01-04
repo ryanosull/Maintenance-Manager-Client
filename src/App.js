429ebcc6e560ae435aa6d7ae14442e4269acbd6b
Ryan <wrosullivan88@gmail.com>
2023-01-04 13:32:25 -0600

    back to backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
-
 import Header from "./Header";
+import Sidebar from  "./Sidebar";
 
 
 function App() {
@@ -9,10 +9,8 @@ function App() {
 
   return (
     <div>
-      
       <Header />
-      
-      
+      <Sidebar />
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Sidebar.css"
+
+
+
+function Sidebar () {
+
+
+
+
+    return (
+        <div id="sidebar">
+            <div className="sidebarText">
+                Unit Overview
+            </div>
+            <div className="sidebarText">
+                New Maintenance Request
+            </div>
+            <div className="sidebarText">
+                Open Requests
+            </div>
+            <div className="sidebarText">
+                Closed Requests
+            </div>
+        </div>
+    )
+
+
+
+};
+
+export default Sidebar
