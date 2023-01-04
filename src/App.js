eed79efb1f5c7d29f457960f0a6e45289a173a0b
Ryan <wrosullivan88@gmail.com>
2023-01-03 22:04:26 -0600

    starting out, use new way to build darkmode button next time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React, {useState} from "react";
 import './App.css';
+import Header from "./Header";
+
 
 function App() {
+
+  const[darkMode, setDarkMode] = useState(false)
+
+  const handleDarkMode =()=> {setDarkMode(darkMode => !darkMode)}
+
+  const appMode = darkMode ? "App dark" : "App light";
+//not sure if appMode is necessary
+
+
+
+
+
   return (
-    <div className="App">
-          hello fucker
+    <div className={`${appMode}`}>
+      <button className="darkmode-Button" onClick= {handleDarkMode}>{darkMode ? " Too bright? 🌕" : "Too dark? 🌑"} </button> 
+      <Header/>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+function Header () {
+
+
+
+
+
+
+    return (
+        <div  >
+            Maintenance Manager v0.01
+        </div>
+    )
+
+
+}
+
+export default Header
