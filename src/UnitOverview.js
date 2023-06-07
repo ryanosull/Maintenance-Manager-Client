f17b08fba2526a1ee3d65d17754eb2b1d147b35f
William Ryan OSullivan <wrosullivan@gmail.com>
2023-06-06 21:31:00 -0500

    units overview cleanedup

--- src/UnitOverview.js
+++ src/UnitOverview.js
@@ -1,36 +1,50 @@
 import React, {useState, useEffect} from "react";
 
 
-const unitsUrl = "http://localhost:9292/units"
+const unitsUrl = "http://localhost:9292/units";
+const ownersUrl = "http://localhost:9292/owners";
 
 function UnitOverview () {
 
-    const [units, setUnits] = useState([])
+    //setting state
+    const [units, setUnits] = useState([]) ;
+    const [owners, setOwners] = useState([]);
 
-    useEffect(() => {
+    useEffect(() => { //fetch units
         fetch(unitsUrl)
         .then(resp => resp.json())
         .then(unitData => setUnits(unitData))
-    }, [])
+    }, []);
 
-    console.log(units)
+    useEffect(() => { //fetch owners
+        fetch(ownersUrl)
+        .then(resp => resp.json())
+        .then(ownerData => setOwners(ownerData))
+    }, []);
+
+    // console.log(owners)
+
 
     return (
-        <div>
-            <table>
+        <div id="tableContainer">
+            <table id="unitsOverviewTable">
                 <thead>
                     <tr>
                         <th>Owner ID</th>
+                        <th>Owner Name</th>
+                        <th>Owner Phone</th>
                         <th>Address</th>
                         <th>Current Tenant</th>
                         <th>Current Open Request?</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {units.map((unit) => {
+                    {units.map((unit) => { //chaining syntax use below for name and phone_number
                         return (
                         <tr key={unit.id}>
                             <td>{unit.owner_id}</td>
+                            <td>{owners.find((owner) => owner.id === unit.owner_id)?.name || null}</td> 
+                            <td>{owners.find((owner) => owner.id === unit.owner_id)?.phone_number || null}</td> 
                             <td>{unit.address}</td>
                             <td>{unit.current_tenant}</td>
                             <td>{unit["open_request?"] ? "✅" : "✖️"}</td> 
